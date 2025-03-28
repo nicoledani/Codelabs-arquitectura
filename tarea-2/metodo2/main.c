2a8832f1441f531a174be677e06b24e158c26bf6
Nicolle Valverde <nicolle.valverde01@uceva.edu.co>
2025-03-28 14:44:54 +0000

    Update Tarea-2

--- tarea-2/metodo2/main.c
+++ tarea-2/metodo2/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double seno (double, double);
+double seno (double, double input);
 
 int main(){
 
 	double grados_a_grad = 0.017453292519943295;
-	double angulo = 45.00;
+//	double angulo = 45.00;
+	double angulo;
 	double resultado;
+
+	printf("Ingrese El Angulo Para Seno(): ");
+	scanf("%lf", &angulo);
 
 	resultado = seno(grados_a_grad, angulo);
 	printf("El seno de %.2f grados es: %.6f\n", angulo, resultado);
